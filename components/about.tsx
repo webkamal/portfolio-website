f169e01074170ef0971abb56d4a538567f420824
Kamal Khanal <ozzykamal@gmail.com>
2025-03-27 03:22:14 +1100

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -20,7 +20,7 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
+        <span className="font-medium">Information Technology</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
@@ -38,11 +38,11 @@ export default function About() {
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        guitar, watching movies, and playing with my kid. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">yoga and meditation</span>. I'm also
+        learning how to play the guitar better.
       </p>
     </motion.section>
   );
