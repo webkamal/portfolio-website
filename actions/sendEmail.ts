2c367f834423c5529e0ebc64fa92cd6ef1888071
Kamal Khanal <ozzykamal@gmail.com>
2025-03-27 03:07:29 +1100

    Update sendEmail.ts
    updated from and to emails to kamalkhanal's emails

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,8 +26,8 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
+      from: "Contact Form <admin@kamalkhanal.com>",
+      to: "hello@kamalkhanal.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
